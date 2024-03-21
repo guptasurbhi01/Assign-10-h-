98e3a4b5199ea5456b6017212caf854f1a41730d
Surbhi gupta <surbhigupta676@gmail.com>
2024-03-21 22:36:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css"
+import { Container, Form, Button, Card, Alert } from "react-bootstrap";
 
-function App() {
+const App = () => {
+  const [textInput, setTextInput] = useState("");
+  const [generatedImage, setGeneratedImage] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://api-inference.huggingface.co/models/prompthero/openjourney-v4",
+        { inputs: textInput },
+        {
+          headers: {
+            Authorization: `Bearer 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`,
+          },
+          responseType: "blob", 
+        }
+      );
+      const string = URL.createObjectURL(response.data);
+      console.log( string);
+      setGeneratedImage(string);
+      setError("");
+    } catch (error) {
+      console.error("Error generating image:", error);
+      setError("Error generating image. Please try again.");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <h1 className="text-center mb-4">AI Image Generation App</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="textInput">
+          <Form.Label>Enter Text:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter text for image generation"
+            value={textInput}
+            onChange={(e) => setTextInput(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" className="mb-3">
+          Generate Image
+        </Button>
+      </Form>
+      {error && <Alert variant="danger">{error}</Alert>}
+      {generatedImage && (
+        <Card style={{ width: "18rem", margin: "0 auto" }}>
+          <Card.Img variant="top" src={generatedImage} />
+        </Card>
+      )}
+    </Container>
   );
-}
+};
 
 export default App;
